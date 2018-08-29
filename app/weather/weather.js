72ae4a85d6ce83c577ac02d19fda0fbaaff0979d
Morten P. JOhansen <mortenpj@gmail.com>
2018-08-29 12:32:10 +0200

    Added simple errorhandling, so atleast the user will be notified when the weather is not updated

--- app/weather/weather.js
+++ app/weather/weather.js
@@ -4,22 +4,50 @@ angular.module('weatherModule', ["openWeatherApiModule"])
 .controller('weatherController', ['$scope', '$rootScope', 'openWeatherApiFactory', function($scope, $rootScope, openWeatherApiFactory) {
 
     $scope.lastweather = {
-        oslo: {lastUpdated: null, weather: null},
+        oslo: {lastUpdated: null, weather: null, error: null},
         london: {lastUpdated: null, weather: null},
         minsk: {lastUpdated: null, weather: null}
     }
 
+    $scope.errors = {
+        oslo: { message: "Failed to update weather for Oslo", showmessage: false},
+        london: { message: "Failed to update weather for London", showmessage: false},
+        minsk: { message: "Failed to update weather for Minsk", showmessage: false}
+    }
+
     $rootScope.$on("OSLO_WEATHER_UPDATED", function(){
+        $scope.errors.oslo.showmessage = false;
+        $scope.lastweather.oslo.lastUpdated = moment().format("HH:mm:ss")
         $scope.lastweather.oslo.weather = openWeatherApiFactory.lastWeather.Oslo;
     })
 
+    $rootScope.$on("OSLO_WEATHER_FAILED", function(){
+        $scope.errors.oslo.showmessage = true;
+    })
+
     $rootScope.$on("LONDON_WEATHER_UPDATED", function(){
+        $scope.errors.london.showmessage = false;
+        $scope.lastweather.london.lastUpdated = moment().format("HH:mm:ss")
         $scope.lastweather.london.weather = openWeatherApiFactory.lastWeather.London;
     })
 
+    $rootScope.$on("LONDON_WEATHER_FAILED", function(){
+        $scope.errors.london.showmessage = true;
+    })
+
     $rootScope.$on("MINSK_WEATHER_UPDATED", function(){
+        $scope.errors.minsk.showmessage = false;
+        $scope.lastweather.minsk.lastUpdated = moment().format("HH:mm:ss")
         $scope.lastweather.minsk.weather = openWeatherApiFactory.lastWeather.Minsk;
     })
+
+    $rootScope.$on("MINSK_WEATHER_FAILED", function(){
+        $scope.errors.minsk.showmessage = true;
+    })
+
+    $scope.refreshOslo = function(){
+        openWeatherApiFactory.getOsloWeather();
+    }
 
     $scope.init = function() {
         $scope.lastweather.oslo.weather = openWeatherApiFactory.lastWeather.Oslo;

b7bc499d207a0bf0516a4a2e57f4934c498b571f
Morten P. JOhansen <mortenpj@gmail.com>
2018-08-29 10:38:55 +0200

    Added methods for getting London and Minsk weather

--- app/weather/openweatherapi.js
+++ app/weather/openweatherapi.js
@@ -4,6 +4,7 @@ angular.module('openWeatherApiModule', [])
 
 .factory('openWeatherApiFactory', ['$rootScope', '$http', function($rootScope, $http) {
 
+
     var service = {
         baseUrl: "https://api.openweathermap.org/data/2.5/",
         appId: "8690db496c1d889b69c90a193971b6f6",
@@ -20,18 +21,69 @@ angular.module('openWeatherApiModule', [])
     };
 
     service.getOsloWeather = function(){
-
-    console.log("Henter oslo vær");
         $http({
           method: 'GET',
           url: service.baseUrl + 'weather?id=' + service.cityId.Oslo + "&appid=" + service.appId
-        }).then(function successCallback(response) {
-            service.lastWeather.Oslo = response.data;
+        })
+        .then(function successCallback(response) {
+            service.lastWeather.Oslo = service.transformToWeatherObject(response.data);
             $rootScope.$broadcast("OSLO_WEATHER_UPDATED");
+        }, function errorCallback(response) {
+            $rootScope.$broadcast("OSLO_WEATHER_FAILED");
+        });
+    }
+
+    service.getLondonWeather = function(){
+        $http({
+          method: 'GET',
+          url: service.baseUrl + 'weather?id=' + service.cityId.London + "&appid=" + service.appId
+        })
+        .then(function successCallback(response) {
+            service.lastWeather.London = service.transformToWeatherObject(response.data);
+            $rootScope.$broadcast("LONDON_WEATHER_UPDATED");
+        }, function errorCallback(response) {
+            $rootScope.$broadcast("LONDON_WEATHER_FAILED");
+        });
+    }
+
+    service.getMinskWeather = function(){
+        $http({
+          method: 'GET',
+          url: service.baseUrl + 'weather?id=' + service.cityId.Minsk + "&appid=" + service.appId
+        }).then(function successCallback(response) {
+            service.lastWeather.Minsk = service.transformToWeatherObject(response.data);
+            $rootScope.$broadcast("MINSK_WEATHER_UPDATED");
           }, function errorCallback(response) {
-            // called asynchronously if an error occurs
-            // or server returns response with an error status.
+            $rootScope.$broadcast("MINSK_WEATHER_FAILED");
           });
+    }
+
+    service.transformToWeatherObject = function(openWeatherapiObject){
+        var weatherObject = {
+                temperature: service.kelvinTocelsius(openWeatherapiObject.main.temp),
+                pressure: openWeatherapiObject.main.preassure,
+                humidity: openWeatherapiObject.main.humidity,
+                weatherDescription: service.getWeatherDescription(openWeatherapiObject.weather)
+        };
+
+        return weatherObject;
+    }
+
+    service.kelvinTocelsius = function(kelvin){
+        return kelvin - 273.15;
+    }
+
+    service.getWeatherDescription = function(weatherArray){
+        var description = "";
+        if(weatherArray.length > 0){
+            for(var i = 0; i < weatherArray.length; i++){
+                description += weatherArray[i].description + ', ';
+            }
+        }
+        if(description.length > 0){
+            description = description.slice(0, description.length -2);
+        }
+        return description;
     }
 
     return service;
